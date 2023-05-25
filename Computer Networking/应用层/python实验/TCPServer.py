a28e26ec35a75e6d3a024931aa00180385ee0362
muyuzhang <2755288579@qq.com>
2023-05-25 22:48:17 +0800

    init

---
+++ Computer Networking/应用层/python实验/TCPServer.py
@@ -0,0 +1,16 @@
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print("The server is ready to receive")
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+    print(capitalizedSentence)
+    connectionSocket.close()
+    if(capitalizedSentence == "EXIT"):
+        print("server is close")
+        break
